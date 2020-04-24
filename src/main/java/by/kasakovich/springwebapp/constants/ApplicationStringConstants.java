d3673eecc1348364d62080d7ffb91d7caf3af506
SlarVer <mkasakovich@mail.ru>
2020-04-24 13:31:50 +0300

    Updated launch config

--- src/main/java/by/kasakovich/springwebapp/constants/ApplicationStringConstants.java
+++ src/main/java/by/kasakovich/springwebapp/constants/ApplicationStringConstants.java
@@ -25,7 +25,7 @@ public class ApplicationStringConstants {
     public static final String QUERY_COLUMN_USER ="user";
     public static final String QUERY_COLUMN_RESULT ="result";
 
-    public static final String SQL_PATH = "C:\\Work\\Projects\\springwebapp\\src\\main\\resources\\sql\\firstInitialization.sql";
+    public static final String SQL_PATH = "../../src/main/resources/sql/firstInitialization.sql";
 
     public static final String MODEL ="model";
 
